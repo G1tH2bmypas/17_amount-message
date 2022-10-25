e64485fff568c905020ef147426756e512a1322a
Tekyto <Tekyto2014@gmail.com>
2022-10-25 14:15:10 +0300

    fixed

--- index.cpp
+++ index.cpp
@@ -5,7 +5,7 @@
 std::string message(int (&numbers)[5]) {
 	int num = 0;
 	for (int i = 0; i < 5; i++) {
-		num = num += numbers[i];
+		num += numbers[i];
 	}
 	return "amount: " + std::to_string(num);
 }
